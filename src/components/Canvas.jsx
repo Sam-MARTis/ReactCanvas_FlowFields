e4fae36a849e81068f3c9ed1027a748e36bd2631
Sam-MARTis <samanthmartis@gmail.com>
2024-02-28 23:58:06 +0530

    first canvas drawing in react

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,14 +2,25 @@ import { useRef, useEffect } from "react";
 
 const Canvas = (props) => {
   const canvasRef = useRef(null);
-
+  useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    
+      const context = canvas.getContext("2d"); 
+      context.fillStyle = "white";
+      context.beginPath();
+      context.fillStyle = "white";
+      
+      context.lineTo(100, 100);
+      context.stroke();
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
-    return (
-        <>
-            <canvas ref={canvasRef} {...props} />
-        </>
-    )
+      
+  }, []);
+
+  return (
+    <>
+      <canvas ref={canvasRef} {...props} />
+    </>
+  );
 };
+
+export default Canvas
