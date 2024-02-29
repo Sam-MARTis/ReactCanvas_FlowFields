78555b784be1771a036d4ee60e5be27ba2d8bac3
Sam-MARTis <samanthmartis@gmail.com>
2024-02-29 21:49:49 +0530

    did basic drawing

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -3,17 +3,9 @@ import { useRef, useEffect } from "react";
 const Canvas = (props) => {
   const canvasRef = useRef(null);
   useEffect(() => {
-    const canvas = canvasRef.current;
-      const context = canvas.getContext("2d"); 
-      context.fillStyle = "white";
-      context.beginPath();
-      context.fillStyle = "white";
-      
-      context.lineTo(100, 100);
-      context.stroke();
-      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    doCanvas(canvasRef)
 
-      
+    // context.fillRect(0, 0, context.canvas.width, context.canvas.height);
   }, []);
 
   return (
@@ -23,4 +15,61 @@ const Canvas = (props) => {
   );
 };
 
-export default Canvas
+const doCanvas = (canvasReference) => {
+  const canvas = canvasReference.current;
+  const pixelRatio = devicePixelRatio || 1;
+  canvas.style.width = window.innerWidth + "px";
+  canvas.style.height = window.innerHeight + "px";
+  const width = window.innerWidth * pixelRatio;
+  const height = window.innerHeight * pixelRatio;
+  canvas.width = width;
+  canvas.height =height;
+  canvas.style.backgroundColor="black";
+
+  const context = canvas.getContext("2d");
+
+  const cellSize = 100;
+
+  // const cellHorizCount = width / cellSize;
+  // const cellVertCount = height / cellSize;
+
+
+
+  context.beginPath();
+  let delX;
+  let delY;
+
+  for (let i = 0; i < width; i += cellSize) {
+    for (let j = 0; j < height; j += cellSize) {
+      context.strokeStyle = "red";
+      context.moveTo(i, j);
+      delX = Math.sin(i / cellSize)*cellSize;
+      delY = Math.cos(i / cellSize)*cellSize;
+      context.lineTo(i + delX, j + delY);
+      context.stroke();
+      
+      
+      
+    }
+  }
+
+
+
+
+  // context.strokeStyle = "red";
+  // // context.beginPath();
+  // context.strokeStyle = "red";
+  // context.moveTo(100, 100);
+  // context.lineTo(300, 400);
+  // context.stroke();
+
+}
+
+
+
+
+
+
+
+
+export default Canvas;
